697edab1b8c3c63fc0e3282c68b522efaa00932c
Sixtine Gennart <seize06@hotmail.com>
2017-01-14 23:50:24 +0100

    AnimSuiteP2

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    float time;
+	// Use this for initialization
+	void Start () {
+        time = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log( Time.time - time );
+	}
+}
