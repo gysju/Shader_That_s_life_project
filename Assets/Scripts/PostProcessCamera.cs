b83c3360f92b4f6bfa48cda8dd0f57afce71bf91
Sixtine Gennart <seize06@hotmail.com>
2017-01-15 17:09:55 +0100

    tweak shaders values

--- Assets/Scripts/PostProcessCamera.cs
+++ Assets/Scripts/PostProcessCamera.cs
@@ -8,6 +8,17 @@ public class PostProcessCamera : MonoBehaviour {
     public Material secondeEffect;
 
     public bool useFirstEffect = true;
+
+    [HeaderAttribute("effect1")]
+
+    public float Aberation = 0.0f;
+    public Vector2 AberationDir = Vector2.zero;
+
+    [HeaderAttribute("effect2")]
+    public float Speed = 0;
+    public float Amplitude = 0;
+    public float Frequence = 0;
+
     void OnRenderImage( RenderTexture source, RenderTexture destination)
 	{
         if( useFirstEffect == true)
@@ -16,5 +27,20 @@ public class PostProcessCamera : MonoBehaviour {
         }
         else
             Graphics.Blit(source, destination, secondeEffect);
+    }
+
+    void Update()
+    {
+        if (useFirstEffect == true)
+        {
+            firstEffect.SetFloat("_Aberations", Aberation);
+            firstEffect.SetVector("_AberationsDir", new Vector4(AberationDir.x, AberationDir.y));
+        }
+        else
+        {
+            secondeEffect.SetFloat("_WaveSpeed", Speed);
+            secondeEffect.SetFloat("_WaveAmplitude", Amplitude);
+            secondeEffect.SetFloat("_WaveFrequence", Frequence);
+        }
     }
 }
